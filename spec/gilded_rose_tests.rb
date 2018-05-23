3e0ff1017b8b155ac09fdcb3f2d3dcd87c95fbe4
marydomashneva <mary.domashneva@gmail.com>
2018-05-23 12:59:53 +0100

    Passing first test

--- spec/gilded_rose_tests.rb
+++ spec/gilded_rose_tests.rb
@@ -1,10 +1,10 @@
-require File.join(File.dirname(__FILE__), './lib/gilded_rose')
+require File.join(File.dirname(__FILE__), '../lib/gilded_rose')
 require 'test/unit'
 
 class TestUntitled < Test::Unit::TestCase
 
   def test_foo
-    items = [Item.new("foo", 0, 0)]
+    items = [Item.new("fixme", 0, 0)]
     GildedRose.new(items).update_quality()
     assert_equal items[0].name, "fixme"
   end
