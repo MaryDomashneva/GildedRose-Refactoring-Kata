bff64069923512add4ee09cd96f21e7697e2ecae
marydomashneva <mary.domashneva@gmail.com>
2018-05-23 15:24:43 +0100

    string class test

--- spec/string_spec.rb
+++ spec/string_spec.rb
@@ -5,4 +5,9 @@ describe String do
     expect(subject).to respond_to(:create_string).with(1).argument
   end
 
+  it 'creats a string' do
+    new_item = Item.new('Club_mate', 30, 50)
+    expect(subject.create_string(new_item)).to eq("Club_mate, 30, 50")
+  end
+
 end
