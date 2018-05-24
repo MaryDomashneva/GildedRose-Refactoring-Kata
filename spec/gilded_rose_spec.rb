f5eff726f55aca7504d8366f7a3816186b709284
marydomashneva <mary.domashneva@gmail.com>
2018-05-24 15:24:16 +0100

    Test coverd for class GildedRose

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -2,16 +2,40 @@ require File.join(File.dirname(__FILE__), '../lib/gilded_rose')
 require File.join(File.dirname(__FILE__), '../lib/item_categories')
 
 describe GildedRose do
+  let(:normal_item) { Item.new('Club_mate', 30, 50) }
+  let(:ind_item) { Item.new('Sulfuras', 30, 80) }
+  let(:back_item) { Item.new('Backstage passes', 30, 50) }
+  let(:aged_item) { Item.new('Aged Brie', 30, 50) }
+  let(:fast_item) { Item.new('Conjured', 30, 50) }
 
   describe '#initialize' do
-    it 'create a correct odject' do
-      new_item = Item.new('Club_mate', 30, 50)
-      gilden_rose = GildedRose.new(new_item)
-      expect(gilden_rose.items).to eq(new_item)
+    it 'creates a correct object' do
+      new_items = [
+        normal_item,
+        ind_item,
+        back_item,
+        aged_item,
+        fast_item
+      ]
+      gildedRose = GildedRose.new(new_items)
+      expect(gildedRose.items).to eq(new_items)
     end
   end
 
   describe '#update_quality' do
-
+    it 'updates quality' do
+      new_items = [
+        normal_item,
+        ind_item,
+        back_item,
+        aged_item,
+        fast_item
+      ]
+      gildedRose = GildedRose.new(new_items)
+      updated_items = gildedRose.update_quality()
+      expect(updated_items[0].name).to eq('Club_mate')
+      expect(updated_items[0].sell_in).to eq(29)
+      expect(updated_items[0].quality).to eq(49)
+    end
   end
 end
